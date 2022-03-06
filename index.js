f5581fdc784a0002e1eb4775bf5fea48b05a36be
DrFitnessapp2018 <drfitnessapp2018@gmail.com>
2022-03-07 01:10:08 +0500

    make it bettersaa

--- index.js
+++ index.js
@@ -64,11 +64,20 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'client/build')));
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname,'client/build','index.html'));
-    res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
-  });
+  // app.use(express.static(path.join(__dirname,'client/build')));
+  // app.get("*", (req, res) => {
+  //   req.sendFile(path.resolve(__dirname,'client/build','index.html'));
+  //   res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+  // });
+
+  // if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  // }
 }
 
 
