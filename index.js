02b41441fc73092c5bb05c9d69aa153eb56cdd30
DrFitnessapp2018 <drfitnessapp2018@gmail.com>
2022-03-07 00:12:59 +0500

    initialaaaa

--- index.js
+++ index.js
@@ -48,6 +48,21 @@ app.use(express.json());
 app.use(cors());
 
 
+const whitelist = ['http://localhost:3000', 'http://localhost:4000', 'https://drfitness.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,'client/build')));
   app.get("*", (req, res) => {
